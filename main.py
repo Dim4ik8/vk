05a1bcb2f12a747c80545860b9790b6b90386627
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-12-03 12:45:33 +0300

    Get groups from VK

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from pathlib import Path
 import os
-
-url = 'https://xkcd.com/info.0.json'
+from dotenv import load_dotenv
 
 def download_image(url, filename, folder='images'):
     response = requests.get(url)
@@ -12,12 +11,20 @@ def download_image(url, filename, folder='images'):
         file.write(response.content)
 
 def main():
+    load_dotenv()
+    token = os.getenv('VK_TOKEN')
+    # обращаемся к комиксу и выводим в консоль его комментарий
+    url = 'https://xkcd.com/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
-
     comic_book = response.json()
     print(comic_book['alt'])
 
+    # обращаемся к API VK
+    url_vk = 'https://api.vk.com/method/groups.get'
+    params = {'access_token': token, 'v': '5.131'}
+    response_vk = requests.get(url_vk, params=params)
+    print(response_vk.json())
 
 if __name__ == '__main__':
     main()
