2229db1196654362630c6b584a6a3c2591dcca1b
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-12-03 12:16:30 +0300

    Get comment from comic_book

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import requests
+from pathlib import Path
+import os
 
 url = 'https://xkcd.com/info.0.json'
 
+def download_image(url, filename, folder='images'):
+    response = requests.get(url)
+    response.raise_for_status()
+    Path(folder).mkdir(exist_ok=True)
+    with open(f'{os.path.join(folder, filename)}', 'wb') as file:
+        file.write(response.content)
 
 def main():
     response = requests.get(url)
     response.raise_for_status()
 
     comic_book = response.json()
-    print(comic_book)
+    print(comic_book['alt'])
+
 
 if __name__ == '__main__':
     main()
