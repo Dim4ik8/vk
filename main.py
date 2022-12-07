3ed82a99b959146d2e6e6084a38a1cdff2fc367c
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-12-07 14:42:12 +0300

    Upload image to VK

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ def main():
     load_dotenv()
     token = os.getenv('VK_TOKEN')
     # обращаемся к комиксу и выводим в консоль его комментарий
-    # url = 'https://xkcd.com/info.0.json'
-    # response = requests.get(url)
-    # response.raise_for_status()
-    # comic_book = response.json()
-    # print(comic_book['alt'])
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    comic_book = response.json()
+    print(comic_book['alt'])
+    download_image(comic_book['img'], 'image.png')
 
-    # обращаемся к API VK
+    # обращаемся к API VK, выводим информацию о группах
     # url_vk = 'https://api.vk.com/method/groups.get'
     # params = {'access_token': token, 'v': '5.131'}
     # response_vk = requests.get(url_vk, params=params)
@@ -31,6 +32,19 @@ def main():
     params = {'access_token': token, 'v': '5.131', 'group_id': '217553308'}
     response = requests.get(url_vk, params=params)
     print(response.json())
+    upload_url = response.json()['response']['upload_url']
+    print(upload_url)
+
+    with open('images/image.png', 'rb') as file:
+        url = upload_url
+        photo = {
+            'media': file,
+            }
+        response = requests.post(url, data=photo)
+        response.raise_for_status()
+        print(response.json())
+
+
 
 if __name__ == '__main__':
     main()
