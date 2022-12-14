758acb44bc4c5dff63354a23726379f3edf44569
Dim4ik8 <87538630+Dim4ik8@users.noreply.github.com>
2022-12-14 12:38:49 +0300

    Add function instead of all code

--- main.py
+++ main.py
@@ -12,6 +12,51 @@ def download_image(url, filename):
     with open(f"{os.path.join('images', filename)}", 'wb') as file:
         file.write(response.content)
 
+def get_request_for_comic(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    comic_book = response.json()
+    message = comic_book['alt']
+    return comic_book, message
+
+def public_post_to_vk_wall(token, group_id, image, text):
+    vk_url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {'access_token': token, 'v': '5.131', 'group_id': group_id}
+    response = requests.get(vk_url, params=params)
+    upload_url = response.json()['response']['upload_url']
+
+    with open(image, 'rb') as file:
+        response = requests.post(upload_url, files={'photo': file})
+    response.raise_for_status()
+    response_for_public = response.json()
+    vk_server = response_for_public['server']
+    vk_photo = response_for_public['photo']
+    vk_hash = response_for_public['hash']
+
+    save_url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {
+        'access_token': token,
+        'v': '5.131',
+        'group_id': group_id,
+        'photo': vk_photo,
+        'server': vk_server,
+        'hash': vk_hash
+    }
+    save_wall_photo = requests.post(save_url, params=params).json()
+    owner_id = save_wall_photo['response'][0]['owner_id']
+    media_id = save_wall_photo['response'][0]['id']
+    attachments = f'photo{owner_id}_{media_id}'
+    post_url = 'https://api.vk.com/method/wall.post'
+    params = {
+        'access_token': token,
+        'v': '5.131',
+        'owner_id': f'-{group_id}',
+        'from_group': '1',
+        'attachments': attachments,
+        'message': text
+    }
+    requests.post(post_url, params=params)
+
 
 def main():
     load_dotenv()
@@ -20,70 +65,23 @@ def main():
     Path('images').mkdir(exist_ok=True)
     try:
         url = 'https://xkcd.com/info.0.json'
-        response = requests.get(url)
-        response.raise_for_status()
-        comic_book = response.json()
-        message = comic_book['alt']
-        print(f"Комментарий к комиксу: {message}")
-
+        comic_book, message = get_request_for_comic(url)
         download_image(comic_book['img'], 'image.png')
 
         total_comics = comic_book['num']
         num_of_public = random.randint(1, total_comics)
         url = f'https://xkcd.com/{num_of_public}/info.0.json'
-        response = requests.get(url)
-        response.raise_for_status()
-        comic_book = response.json()
-        message = comic_book['alt']
-        print(f"Комментарий к комиксу: {message}")
+        comic_book, message = get_request_for_comic(url)
         download_image(comic_book['img'], f'{num_of_public}.png')
+        image_for_public = os.path.join('images', f'{num_of_public}.png')
 
         vk_url = 'https://api.vk.com/method/groups.get'
         params = {'access_token': token, 'v': '5.131'}
         response_vk = requests.get(vk_url, params=params)
         print(response_vk.json())
 
-        vk_url = 'https://api.vk.com/method/photos.getWallUploadServer'
-        params = {'access_token': token, 'v': '5.131', 'group_id': group_id}
-        response = requests.get(vk_url, params=params)
-        upload_url = response.json()['response']['upload_url']
-        print(f'Ссылка для загрузки фото: {upload_url}')
+        public_post_to_vk_wall(token, group_id, image_for_public, message)
 
-        with open(f'images/{num_of_public}.png', 'rb') as file:
-            response = requests.post(upload_url, files={'photo': file})
-        response.raise_for_status()
-        response_for_public = response.json()
-        vk_server = response_for_public['server']
-        vk_photo = response_for_public['photo']
-        vk_hash = response_for_public['hash']
-
-        save_url = 'https://api.vk.com/method/photos.saveWallPhoto'
-        params = {
-            'access_token': token,
-            'v': '5.131',
-            'group_id': group_id,
-            'photo': vk_photo,
-            'server': vk_server,
-            'hash': vk_hash
-        }
-        save_wall_photo = requests.post(save_url, params=params).json()
-        print(f'Ответ от saveWallPhoto: {save_wall_photo}')
-
-        owner_id = save_wall_photo['response'][0]['owner_id']
-        media_id = save_wall_photo['response'][0]['id']
-        attachments = f'photo{owner_id}_{media_id}'
-        print(attachments)
-        post_url = 'https://api.vk.com/method/wall.post'
-        params = {
-            'access_token': token,
-            'v': '5.131',
-            'owner_id': f'-{group_id}',
-            'from_group': '1',
-            'attachments': attachments,
-            'message': message
-        }
-        print(params)
-        requests.post(post_url, params=params)
     finally:
         shutil.rmtree('images')
 
